e9be55de626f5cd47ecd133762902ede676493a9
Monikathanki <thanki.monika2@gmail.com>
2020-09-17 20:43:41 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AddItemForm from './AddItemForm';
+import ShoppingList from './ShoppingList';
+import ShoppingItem from './ShoppingItem';
+import './App.css';
+
+
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm />
+          </section>
+          <section>
+            <ShoppingList items={this.state.shoppingItems} />
+          </section>
+        </main>
+      </>
+    )
+  }
+}
+
+
+
+export default App;
